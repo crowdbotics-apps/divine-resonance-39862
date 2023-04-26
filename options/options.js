55a90db466fdc4e909461ec6d089ab5933c88b0f
Slobodan Petrovic <slobodan.petrovic@crowdbotics.com>
2023-04-26 21:43:04 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "divine-resonance-39862",
-  url: "https://divine-resonance-39862.botics.co",
-  api: "https://divine-resonance-39862.botics.co/api/v1"
+  "name": "divine-resonance-39862",
+  "url": "https://divine-resonance-39862.botics.co",
+  "api": "https://divine-resonance-39862.botics.co/api/v1",
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
